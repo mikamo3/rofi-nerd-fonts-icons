98c43e4fccc452df6f9516c921a0bb5ecbdcb720
mikamo3 <kamo3proj@gmail.com>
2019-01-16 00:21:05 +0900

    remove testcode

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import re
 url = 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/css/nerd-fonts-generated.css'
 res = requests.get(url)
 style = parseString(res.text)
-i = 0
 for rule in style.cssRules:
-    i += 1
-    if i == 10:
-        break
     if (rule.__class__.__name__ == "CSSStyleRule"):
         match_fc_name = re.match(r'^(\.nf-.*?):before', rule.selectorText)
         if match_fc_name:
